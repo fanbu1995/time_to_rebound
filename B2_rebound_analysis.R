9e6d758d2fba2857dcc4a444c1c4813b05503d9e
fb75@duke.edu <fb75@duke.edu>
2020-05-28 12:05:47 -0400

    create B2 analysis script

---
+++ B2_rebound_analysis.R
@@ -0,0 +1,7 @@
+# 05/28/2020
+# first look at the B2 rebound data (cleaned by Yishu Gong)
+
+library(tidyverse)
+library(survival)
+
+setwd("~/Documents/Research_and_References/HIV_rebound_summer2020/")
