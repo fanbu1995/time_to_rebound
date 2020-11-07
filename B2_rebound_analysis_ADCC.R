bb40b0e7a12bb300429f00d44b8f2a3bb88c34a6
fb75@duke.edu <fb75@duke.edu>
2020-11-06 22:20:00 -0500

    update analysis with ADCC and ADCP

--- B2_rebound_analysis_ADCC.R
+++ B2_rebound_analysis_ADCC.R
@@ -68,10 +68,6 @@ C_stats %>% filter(Predictor %in%  c('ADCC_week56', 'ADCP_week62'))
 # ADCP: 0.500 (ranked #28 among 43)
 
 
-### Not very important...
-# log_Abs_CD4_week8 and log_Abs_CD8_week8 ranked at 12 and 13
-# they are the only ones with >0.5 C-stats
-
 
 # 3. Try out "Coxnet": using the `glmnet` package
 # 3.1 get the design matrix 
@@ -183,6 +179,26 @@ dat_log_sel = dat_log %>%
 ## right now: 23 predictors
 X = dat_log_sel[,3:25] %>% as.matrix()
 
+## (11/5/2020)
+# add in ADCC and ADCP!
+dat_log_sel = dat_log %>% 
+  select(rebound_time_days_post_ati, observed,
+         log_peak_vl_2, log_vl_treat,
+         log_peak_gp41, log_peak_gp120,
+         log_RNA_copies_blood_56, log_RNA_copies_LN_56,
+         log_RNA_copies_RB_56, 
+         log_DNA_copies_Blood_16:log_DNA_copies_Blood_56,
+         log_DNA_copies_LN_36, log_DNA_copies_LN_56,
+         log_DNA_copies_RB_16:log_DNA_copies_RB_56,
+         pos_auc_0_weeks_post_ATI, pos_auc_4_weeks_post_ATI,
+         pos_auc_8_weeks_post_ATI,
+         log_Abs_CD4_week0:log_Abs_CD4_week8,
+         Challenge_times, Dosage, 
+         ADCC_week56, ADCP_week62)
+
+## now: 25 predictors in the pool
+X = dat_log_sel[,3:27] %>% as.matrix()
+
 
 # 4.2 fit Lasso (alpha=1)
 phmod_lasso = glmnet(X, 
@@ -201,6 +217,8 @@ plot(cv_phmod_lasso1)
 ### BUT! CV with 10 observations isn't very reliable
 ### Probably can only get "promising predictors"
 
+### New on 11/05/2020: pretty much the same, 1~4 predictors
+
 ## (using concordance, 5-fold)
 cv_phmod_lasso2 = cv.glmnet(X, 
                             Surv(dat_log_sel$rebound_time_days_post_ati, 
@@ -211,9 +229,13 @@ plot(cv_phmod_lasso2)
 ### also, somewhere between between 1 and 4 predictors: 
 ### C = close to 1
 
+### New on 11/05/2020: 2 predictors seem best
+
 ## extract coefficients using the 1st CV results
 cv_phmod_lasso1$lambda.min
 # [1] 0.3589552
+# new 11/05/2020: 
+# [1] 0.2254343
 coefficients <- coef(phmod_lasso, s = cv_phmod_lasso1$lambda.min)
 active_index <- which(coefficients != 0)
 active_coefficients <-coefficients[active_index]
@@ -227,11 +249,20 @@ cv_res
 # 1           log_gp41_treat -0.003705671
 # 2 pos_auc_0_weeks_post_ATI -6.054194923
 
+## New on 11/05/2020
+#                 Predictor    Coefficient
+# 1  log_RNA_copies_blood_56  -0.04274380
+# 2 pos_auc_0_weeks_post_ATI -11.48880921
+# 3        log_Abs_CD4_week8  -0.08234868
+# 4          Challenge_times  -0.02683582
+# 5              ADCC_week56  -0.06270668
+
+
 # 4.3 Use Lasso results to obtain a "predictor inclusion ranking"
 pred_in = NULL
 coef_sign = NULL
 
-for(l in cv_phmod_lasso1$lambda){
+for(l in phmod_lasso$lambda){
   coefficients = coef(phmod_lasso, s = l)
   active_index = which(coefficients != 0)
   active_coefficients = coefficients[active_index]
@@ -240,6 +271,8 @@ for(l in cv_phmod_lasso1$lambda){
   #cat(active_predictors, "\n")
   
   if(any(!active_predictors %in% pred_in)){
+    cat('Added new predictors! Now active:', active_predictors, '\n')
+    
     new_index = which(!active_predictors %in% pred_in)
     pred_in = c(pred_in, active_predictors[new_index])
     new_coef_sign = ifelse(active_coefficients[new_index] > 0, 
@@ -247,6 +280,15 @@ for(l in cv_phmod_lasso1$lambda){
     coef_sign = c(coef_sign, new_coef_sign)
   }
 }
+
+## 11/05/2020
+## when lambda changes, we may end up selecting slightly different combos
+## of variables than before (so it's not exactly cumulative)
+## so only focus on all the 9 variables that eventually got selected with 
+## min lambda
+
+coef_sign = coef_sign[pred_in %in% active_predictors]
+pred_in = pred_in[pred_in %in% active_predictors]
 
 ## get a vector of "effect on rebound"
 ## (+: accelerate; -: delay)
@@ -306,6 +348,13 @@ for(l in cv_phmod_lasso3$lambda){
   }
 }
 
+## 11/05/2020
+## same as before...
+## only focus on all the 9 variables that eventually got selected with 
+## min lambda
+LOO_deviance = LOO_deviance[pred_in2 %in% active_predictors]
+pred_in2 = pred_in2[pred_in2 %in% active_predictors]
+
 
 ## put together a summary table of this thing
 predictor_inclusion = data.frame(Inclusion_Rank = c(1:9),
@@ -316,6 +365,18 @@ predictor_inclusion = data.frame(Inclusion_Rank = c(1:9),
                                  Cum_concordance = Cum_C_stats,
                                  LOOCV_deviance = LOO_deviance)
 predictor_inclusion
+
+## get a latex version of this table
+library(xtable)
+## only include the columns that my slide can hold
+predictor_inclusion  = predictor_inclusion %>%
+  select(Predictor, 
+         Effect = Rebound_Effect, 
+         Cum.Concord = Cum_concordance,
+         LOOCV_deviance)
+
+
+xtable(predictor_inclusion, digits = 3)
 
 
 ## 07/13/2020
@@ -330,16 +391,36 @@ summary(phmod_auc0_gp41)
 # Wald test            = 6.62  on 2 df,   p=0.04
 # Score (logrank) test = 8.74  on 2 df,   p=0.01
 
+## 11/05/2020
+## the "chosen" model now includes ADCC_week56
+phmod_auc0_gp41_adcc = coxph(Surv(rebound_time_days_post_ati, observed) ~ 
+                               pos_auc_0_weeks_post_ATI + log_peak_gp41 +
+                               ADCC_week56, 
+                             data = dat_log)
+summary(phmod_auc0_gp41_adcc)
+
+## try out only AUC_0 and ADCC_week56??
+phmod_auc0_adcc = coxph(Surv(rebound_time_days_post_ati, observed) ~ 
+                          pos_auc_0_weeks_post_ATI + ADCC_week56, 
+                        data = dat_log)
+summary(phmod_auc0_adcc)
+
+# a bit more "significant" in terms of Wald test? 
+## but ONLY AUC0 is significant at 0.05 level in either model...
+
 
 ## b) survival curves at representative values of each variable
-## i) fix peak GP41 at mean, vary AUC_0_week
+## i) fix peak GP41 and ADCC_week56 at mean, vary AUC_0_week
 mean(dat_log$log_peak_gp41)
 # [1] 4.512378
+mean(dat_log$ADCC_week56)
+# [1] 23.73512
 auc0_values = c(0.15, 0.25, 0.3, 0.4) # 0.25 is approximately the mean
 
-auc0_fit = survfit(phmod_auc0_gp41, 
+auc0_fit = survfit(phmod_auc0_gp41_adcc, 
                    newdata = data.frame(pos_auc_0_weeks_post_ATI = auc0_values,
-                                        log_peak_gp41 = mean(dat_log$log_peak_gp41)))
+                                        log_peak_gp41 = mean(dat_log$log_peak_gp41),
+                                        ADCC_week56 = mean(dat_log$ADCC_week56)))
 ggsurvplot(auc0_fit, conf.int = FALSE, 
            data = dat_log,
            legend = "right",
@@ -348,10 +429,10 @@ ggsurvplot(auc0_fit, conf.int = FALSE,
                          "0.25(mean)",
                          "0.30",
                          "0.40"),
-           caption = "Fix log_peak_gp41 at 4.5 (mean)",
+           caption = "Fix log_peak_gp41 at 4.5, ADCC_week56 at 23.7 (mean)",
            ggtheme = theme_bw(base_size = 14))
 
-## ii) fix AUC_0_week at mean, vary log peak VL
+## ii) fix AUC_0_week and ADCC_week56 at mean, vary log peak gp41
 sort(dat_log$log_peak_gp41)
 # [1] 3.929253 4.147115 4.153393 4.159597 4.495433 4.612036 4.826567 4.836934
 # [9] 4.846614 5.116840
@@ -359,8 +440,9 @@ mean(dat_log$pos_auc_0_weeks_post_ATI)
 # [1] 0.24949
 peakgp41_values = c(4, 4.5, 5) # 4.5 is approximately the mean value
 
-peakgp41_fit = survfit(phmod_auc0_gp41, 
+peakgp41_fit = survfit(phmod_auc0_gp41_adcc, 
                        newdata = data.frame(pos_auc_0_weeks_post_ATI = mean(dat_log$pos_auc_0_weeks_post_ATI),
+                                            ADCC_week56 = mean(dat_log$ADCC_week56),
                                             log_peak_gp41 = peakgp41_values))
 ggsurvplot(peakgp41_fit, conf.int = FALSE, 
            data = dat_log,
@@ -369,5 +451,27 @@ ggsurvplot(peakgp41_fit, conf.int = FALSE,
            legend.labs=c("4.0", 
                          "4.5(mean)",
                          "5.0"),
-           caption = "Fix pos_auc_0_weeks_post_ATI at 0.25 (mean)",
+           caption = "Fix pos_auc_0 at 0.25, ADCC_week56 at 23.7 (mean)",
+           ggtheme = theme_bw(base_size = 14))
+
+## iii) Fix AUC_0 and peak_gp41 at mean values, and vary ADCC_week56
+sort(dat_log$ADCC_week56)
+# [1] 18.65487 18.65487 21.36407 21.36407 21.54801 23.74600 24.35697 25.83522
+# [9] 30.22471 31.60245
+mean(dat_log$ADCC_week56)
+ADCC_values = c(18.5, 23.7, 26, 31.5)
+
+ADCC_fit = survfit(phmod_auc0_gp41_adcc, 
+                   newdata = data.frame(pos_auc_0_weeks_post_ATI = mean(dat_log$pos_auc_0_weeks_post_ATI),
+                                        ADCC_week56 = ADCC_values,
+                                        log_peak_gp41 = mean(dat_log$log_peak_gp41)))
+ggsurvplot(ADCC_fit, conf.int = FALSE, 
+           data = dat_log,
+           legend = "right",
+           legend.title = "ADCC at \nWeek56\n(before ATI)",
+           legend.labs=c("18.5", 
+                         "23.7(mean)",
+                         "26",
+                         "31.5"),
+           caption = "Fix pos_auc_0 at 0.25, log_peak_gp41 at 4.5 (mean)",
            ggtheme = theme_bw(base_size = 14))
